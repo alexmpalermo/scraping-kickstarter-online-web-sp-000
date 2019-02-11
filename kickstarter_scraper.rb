774f1f7ef40116dd5d37fdb078311c053b1d8e38
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-11 21:54:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,5 +5,12 @@ def create_project_hash
  
 kickstarter = Nokogiri::HTML(html)
 
+projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 
+  projects
+end
 end
